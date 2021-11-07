7f326b1e9d2afada61bfe97b4ea64c49c623a13c
Harold Thetiot <hthetiot@gmail.com>
2021-11-07 23:40:43 +0100

    clear jshint errors

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -1,3 +1,5 @@
+/*global describe, it, expect, before */
+
 var TwitterStrategy = require('../lib/strategy')
   , fs = require('fs');
 
@@ -18,8 +20,8 @@ describe('Strategy#userProfile', function() {
       var body = fs.readFileSync('test/fixtures/account/theSeanCook.json', 'utf8');
       var response = {headers:{'x-access-level': 'read'}};
       callback(null, body, response);
-    }
-    
+    };
+
     
     var profile;
   
@@ -67,7 +69,7 @@ describe('Strategy#userProfile', function() {
       var body = fs.readFileSync('test/fixtures/account/theSeanCook.json', 'utf8');
       var response = {headers:{'x-access-level': 'read'}};
       callback(null, body, response);
-    }
+    };
     
     
     var profile;
@@ -116,8 +118,8 @@ describe('Strategy#userProfile', function() {
       var body = fs.readFileSync('test/fixtures/account/theSeanCook.json', 'utf8');
       var response = {headers:{'x-access-level': 'read'}};
       callback(null, body, response);
-    }
-    
+    };
+
     
     var profile;
   
@@ -165,7 +167,7 @@ describe('Strategy#userProfile', function() {
       var body = fs.readFileSync('test/fixtures/account/theSeanCook.json', 'utf8');
       var response = {headers:{'x-access-level': 'read'}};
       callback(null, body, response);
-    }
+    };
     
     
     var profile;
@@ -214,7 +216,7 @@ describe('Strategy#userProfile', function() {
       var body = '{"id_str":"6253282","id":6253282,"profile_text_color":"437792","created_at":"Wed May 23 06:01:13 +0000 2007","contributors_enabled":true,"follow_request_sent":null,"lang":"en","listed_count":10154,"profile_sidebar_border_color":"0094C2","show_all_inline_media":false,"friends_count":34,"utc_offset":-28800,"location":"San Francisco, CA","name":"Twitter API","profile_background_tile":false,"profile_sidebar_fill_color":"a9d9f1","profile_image_url_https":"https:\/\/si0.twimg.com\/profile_images\/1438634086\/avatar_normal.png","protected":false,"geo_enabled":true,"following":null,"default_profile_image":false,"statuses_count":3252,"is_translator":false,"favourites_count":22,"profile_background_color":"e8f2f7","description":"The Real Twitter API. I tweet about API changes, service issues and happily answer questions about Twitter and our API. Do not get an answer? It is on my website.","time_zone":"Pacific Time (US & Canada)","screen_name":"twitterapi","profile_background_image_url":"http:\/\/a0.twimg.com\/profile_background_images\/229557229\/twitterapi-bg.png","profile_image_url":"http:\/\/a0.twimg.com\/profile_images\/1438634086\/avatar_normal.png","profile_link_color":"0094C2","profile_background_image_url_https":"https:\/\/si0.twimg.com\/profile_background_images\/229557229\/twitterapi-bg.png","followers_count":931299,"status":{"in_reply_to_status_id_str":null,"in_reply_to_user_id_str":null,"retweeted":false,"coordinates":null,"in_reply_to_screen_name":null,"created_at":"Tue Feb 14 23:39:43 +0000 2012","possibly_sensitive":false,"contributors":null,"in_reply_to_status_id":null,"entities":{"urls":[{"display_url":"tmblr.co\/ZgBqayGQi3ls","indices":[106,126],"expanded_url":"http:\/\/tmblr.co\/ZgBqayGQi3ls","url":"http:\/\/t.co\/cOzUfFNW"}],"user_mentions":[],"hashtags":[]},"geo":null,"in_reply_to_user_id":null,"place":null,"favorited":false,"truncated":false,"id_str":"169566520693882882","id":169566520693882882,"retweet_count":82,"text":"Photo Upload Issue - Some users may be experiencing an issue when uploading a photo. Our engineers are... http:\/\/t.co\/cOzUfFNW"},"default_profile":false,"notifications":null,"url":"http:\/\/dev.twitter.com","profile_use_background_image":true,"verified":true}';
       var response = {headers:{'x-access-level': 'read'}};
       callback(null, body, response);
-    }
+    };
     
     
     var profile;
@@ -258,13 +260,13 @@ describe('Strategy#userProfile', function() {
     
     strategy._oauth.get = function(url, token, tokenSecret, callback) {
       return callback(new Error('should not fetch profile'));
-    }
+    };
     
     
     var profile;
   
     before(function(done) {
-      strategy.userProfile('token', 'token-secret', {"user_id":"1705","screen_name":"jaredhanson"}, function(err, p) {
+      strategy.userProfile('token', 'token-secret', {'user_id':'1705','screen_name':'jaredhanson'}, function(err, p) {
         if (err) { return done(err); }
         profile = p;
         done();
@@ -278,11 +280,11 @@ describe('Strategy#userProfile', function() {
     });
   
     it('should not set raw property', function() {
-      expect(profile._raw).to.be.undefined;
+      expect(profile._raw).to.be.equal(undefined);
     });
   
     it('should not set json property', function() {
-      expect(profile._json).to.be.undefined;
+      expect(profile._json).to.be.equal(undefined);
     });
   }); // skipping extended profile
   
@@ -296,7 +298,7 @@ describe('Strategy#userProfile', function() {
     strategy._oauth.get = function(url, token, tokenSecret, callback) {
       var body = '{"errors":[{"message":"Invalid or expired token","code":89}]}';
       callback({ statusCode: 401, data: body });
-    }
+    };
     
     
     var err, profile;
@@ -318,7 +320,7 @@ describe('Strategy#userProfile', function() {
     });
     
     it('should not supply profile', function() {
-      expect(profile).to.be.undefined;
+      expect(profile).to.be.equal(undefined);
     });
   }); // error caused by invalid token
   
@@ -332,7 +334,7 @@ describe('Strategy#userProfile', function() {
     strategy._oauth.get = function(url, token, tokenSecret, callback) {
       var body = 'Hello, world.';
       callback(null, body, undefined);
-    }
+    };
     
     
     var err, profile;
@@ -351,7 +353,7 @@ describe('Strategy#userProfile', function() {
     });
     
     it('should not supply profile', function() {
-      expect(profile).to.be.undefined;
+      expect(profile).to.be.equal(undefined);
     });
   }); // error caused by malformed response
   
@@ -364,7 +366,7 @@ describe('Strategy#userProfile', function() {
     
     strategy._oauth.get = function(url, token, tokenSecret, callback) {
       return callback(new Error('something went wrong'));
-    }
+    };
     
     
     var err, profile;
@@ -385,7 +387,7 @@ describe('Strategy#userProfile', function() {
     });
   
     it('should not supply profile', function() {
-      expect(profile).to.be.undefined;
+      expect(profile).to.be.equal(undefined);
     });
   }); // internal error
   
